aae391392267f7dbd6b95ee4f4ebf22cdb37f42e
michel30000 <103256258+michel30000@users.noreply.github.com>
2022-04-10 12:27:05 +0200

    Adding test cases WIP

--- cypress/integration/bynder_ui.js
+++ cypress/integration/bynder_ui.js
@@ -1,5 +1,8 @@
 var locator_input_username = '#inputEmail'
 var locator_input_password = '#inputPassword'
+var locator_link_nederlands = '#languageSwitch > ul > li:nth-child(1) > form > a'
+var locator_link_homepage = '#login > header > div > ul > li > a'
+var locator_link_acceptcookies = '#cookieconsent > div > div > div > div > div.cookiechoices > span > span'
 
 var incorrect_username = makeRandomUsername() //Used to circumvent the robot detector script, sorry :)
 
@@ -12,22 +15,22 @@ describe('Login with correct credentials', function() {
         cy.visit('https://wave-trial.getbynder.com/login/')
 
         cy.get(locator_input_username).should('be.enabled').type(correct_username)
+
         cy.get(locator_input_password).type(correct_password)
 
         cy.contains('Login').click()
 
-        cy.url()
-            .should('include', '/account/dashboard')
+        cy.url().should('include', '/account/dashboard')
         
     })
 })
-
 
 describe('Login with incorrect credentials', function() {
     it('Logs in with incorrect credentials', function() {
         cy.visit('https://wave-trial.getbynder.com/login/')
 
         cy.get(locator_input_username).should('be.enabled').type(incorrect_username)
+
         cy.get(locator_input_password).type('fgsfds')
         
         cy.contains('Login').click()
@@ -36,7 +39,46 @@ describe('Login with incorrect credentials', function() {
     })
 })
 
+describe('Change page language to Dutch', function() {
+    it('Changes the language to Dutch', function() {
+        cy.visit('https://wave-trial.getbynder.com/login/')
+
+        cy.contains('Language').click()
+
+        cy.get(locator_link_nederlands).click()
+
+        cy.contains('Inloggen')
+    })
+})
+
+describe('Click Bynder homepage button', function() {
+    it('Clicks the Bynder homepage button', function() {
+        cy.visit('https://wave-trial.getbynder.com/login/')
+
+        cy.get(locator_link_homepage).click()
+        
+        cy.url().should('include', 'https://www.bynder.com')
+        
+        cy.get(locator_link_acceptcookies).click()
+
+        cy.contains('Copyright 2022 Bynder')
+        
+    })
+})
+
+describe('Contact support with incorrect captcha', function() {
+    it('Contacts support with incorrect captcha', function() {
+        cy.visit('https://wave-trial.getbynder.com/login/')
+        cy.get("#custom-support-form-button").click()
+        cy.contains('Support')
+        
+        
+    })
+})
+
 function makeRandomUsername() {
+    //from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
--- readme.md
+++ readme.md
@@ -1,9 +1,19 @@
 # ui-testing-assignment
 This is my first ever cypress/javascript project. It contains tests for the Bynder trial login page.
 
+## Test scenarios
+* Login with correct credentials
+* Login with incorrect credentials
+* Change page language to Dutch
+* Click Bynder homepage button
+
+
 ## Running the tests
 To run these tests use
 * macOs/linux: `docker run -v $PWD:/test -w /test cypress/included:9.5.3`
 * PowerShell: `docker run -v ${PWD}:/test -w /test cypress/included:9.5.3`
 
 I have only tested this command on macOs, but it should work in Powershell/Windows as long as your docker daemon is running.
+
+## Notes
+I am using the Cypress config option `"chromeWebSecurity": false` to allow the test case "Click Bynder homepage button" to navigate away from the original visit() url.
